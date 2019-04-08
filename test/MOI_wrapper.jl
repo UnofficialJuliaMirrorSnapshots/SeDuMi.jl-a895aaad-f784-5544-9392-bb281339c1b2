1ba961978862c8d2995d0a76071e40dd4b739f6f
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 02:30:48 +0000

    Snapshot of branch master taken on 2019-04-07T22:30:47.341-04:00 from "https://github.com/UnofficialJuliaMirror/SeDuMi.jl-a895aaad-f784-5544-9392-bb281339c1b2"

---
+++ test/MOI_wrapper.jl
@@ -0,0 +1,54 @@
+using Test
+
+using MathOptInterface
+const MOI = MathOptInterface
+const MOIT = MOI.Test
+const MOIU = MOI.Utilities
+const MOIB = MOI.Bridges
+
+import SeDuMi
+const optimizer = SeDuMi.Optimizer(fid=0)
+
+MOIU.@model(ModelData, (), (),
+            (MOI.Zeros, MOI.Nonnegatives, MOI.SecondOrderCone,
+             MOI.RotatedSecondOrderCone, MOI.PositiveSemidefiniteConeTriangle),
+            (), (), (), (MOI.VectorOfVariables,), (MOI.VectorAffineFunction,))
+
+# UniversalFallback is needed for starting values, even if they are ignored by SeDuMi
+const cache = MOIU.UniversalFallback(ModelData{Float64}())
+const cached = MOIU.CachingOptimizer(cache, optimizer)
+
+# Essential bridges that are needed for all tests
+const bridged = MOIB.Vectorize{Float64}(MOIB.NonposToNonneg{Float64}(cached))
+
+@testset "SolverName" begin
+    @test MOI.get(optimizer, MOI.SolverName()) == "SeDuMi"
+end
+
+@testset "supports_allocate_load" begin
+    @test MOIU.supports_allocate_load(optimizer, false)
+    @test !MOIU.supports_allocate_load(optimizer, true)
+end
+
+config = MOIT.TestConfig(atol=1e-4, rtol=1e-4)
+
+@testset "Unit" begin
+    MOIT.unittest(MOIB.SplitInterval{Float64}(bridged),
+                  config,
+                  [# Quadratic functions are not supported
+                   "solve_qcp_edge_cases", "solve_qp_edge_cases",
+                   # Integer and ZeroOne sets are not supported
+                   "solve_integer_edge_cases", "solve_objbound_edge_cases"])
+end
+
+@testset "Continuous linear problems" begin
+    MOIT.contlineartest(MOIB.SplitInterval{Float64}(bridged),
+                        config,
+                        ["linear13"] # See https://github.com/blegat/SeDuMi.jl/issues/7
+                       )
+end
+
+@testset "Continuous conic problems" begin
+    MOIT.contconictest(MOIB.SquarePSD{Float64}(MOIB.RootDet{Float64}(MOIB.GeoMean{Float64}(bridged))),
+                       config, ["rootdets", "exp", "logdet"])
+end
